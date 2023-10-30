0d7a1ccb0e7de32a7fea4d243d11ebcfa40d5637
IvonneConde <ivonneconde91@gmail.com>
2023-10-30 12:57:09 -0600

    comentarios finales

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { validateLinks } = require('./lib/app.js');
 const mdLinks = (filePath, validate = false) => {
   return new Promise((resolve, reject) => {
     if (fs.existsSync(filePath)) {     //identifica si la ruta existe
-      const { absolutePath, extname } = transformToAbs(filePath); // transofrmar la ruta
+      const { absolutePath, extname } = transformToAbs(filePath); // obtiene la ruta absoluta y la extención del archivo
      if (['.md', '.mkd', '.mdwn', '.mdown', '.mdtxt', '.mdtext', '.markdown', '.text'].includes(extname)) {  //verifica que la extensión markdown es válida
     // aquí lee el archivo y lo convierte en string -->
       fs.promises   // modulo para interactuar con el sistema de archivos
@@ -16,11 +16,11 @@ const mdLinks = (filePath, validate = false) => {
     .then((markdownFile) => {
       // para extraer los links y su ruta
       const links = getLinks(markdownFile, absolutePath);
-      if (validate) { 
+      if (validate) {   // Si se debe validar, llama a validateLinks para validar los enlaces
         //validamos los links con validateLinks
         validateLinks(links)
         .then((validateLinks) => {
-          resolve(validateLinks);
+          resolve(validateLinks); // Si no se valida, resuelve con los enlaces sin cambios
         })
         .catch(reject);
       } else {
@@ -31,7 +31,7 @@ const mdLinks = (filePath, validate = false) => {
       reject(error);
     });
     } else {
-      reject('Not a markdown file')}
+      reject('Not a markdown file')}  // Rechaza la promesa si la extensión no es válida
     } else {
       reject('The path doesnt exists');   //sino existe la ruta, rechaza la promesa
     }
